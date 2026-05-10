975b50f8ce79eae01ad97ab07428699eb3d79445
Akshat Koirala <akoirala@macalester.edu>
2026-05-10 11:22:02 -0500

    Refresh stale app bundles on deploy

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './global.css'
 
+declare global {
+  interface Window {
+    __MINDCRAFT_BUILD__?: string
+  }
+}
+
+const buildVersion = window.__MINDCRAFT_BUILD__
+if (buildVersion) {
+  const key = 'mindcraft:last-build'
+  const previousBuild = localStorage.getItem(key)
+  localStorage.setItem(key, buildVersion)
+  if (previousBuild && previousBuild !== buildVersion) {
+    window.location.reload()
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
